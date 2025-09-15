60753b24fd31976c57eb73d6f66f3e520ce710d4
Niharika-Seth <niharikaseth1112@gmail.com>
2025-09-15 22:49:18 +0530

    Updated views.py with member login and logout function

--- gymapp/views.py
+++ gymapp/views.py
@@ -51,6 +51,29 @@ def member_delete(request, pk):
         return redirect('member_list')
     return render(request, 'gymapp/member_delete.html', {'member': member})
 
+# Create login view
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')   # redirect after login
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'gymapp/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')   # redirect to login after logout
+
+
 
 
 
